4d21dfe34214970c91f792d98334c0db26e77dc8
S7venKing <112841486+S7venKing@users.noreply.github.com>
2024-10-08 14:34:16 +0700

    update setup first kctf controller

---
+++ kctf_service/kctf_service/Controllers/KCTFController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace kctf_service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KCTFController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllCluster()
+        {
+            return Ok();
+        }
+    }
+
+
+}
