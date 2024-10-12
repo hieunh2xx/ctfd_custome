b09e4c387dda18cc4164771efa31bff2c1397734
s7venking <3r4n01d@gmail.com>
2024-10-12 18:08:12 +0700

    dockerfile deploy app with db server

---
+++ kctf_service/ctf-directory/my_flask_app/challenge/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+# Cấu hình kết nối MySQL
+db_config = {
+    'host': 'localhost',
+    'user': 'dbuser',
+    'password': '123',  # Thay đổi mật khẩu tương ứng
+    'database': 'testdb'
+}
+
+@app.route('/', methods=['GET'])
+def get_data():
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM sample_table;")
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(rows)
+
+@app.route('/', methods=['POST'])
+def add_data():
+    new_data = request.json.get('data')
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO sample_table (data) VALUES (%s);", (new_data,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return jsonify({"message": "Data added!"}), 201
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
